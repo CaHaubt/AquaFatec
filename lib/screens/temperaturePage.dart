1f2ac6504120e2ee0a1badfea956bd2c47151504
CaHaubt <carol_haubt@hotmail.com>
2024-05-07 22:06:26 -0300

    Acrescentado conexão com o banco de dados e armazenamento das informações de temperatura. By Gio

--- lib/screens/temperaturePage.dart
+++ lib/screens/temperaturePage.dart
@@ -6,13 +6,9 @@ import '../widgets/colors.dart';
 import '../widgets/navibar.dart';
 import 'package:intl/intl.dart';
 import 'package:firebase_database/firebase_database.dart';
-//import '../firebase_options.dart';
-
-final databaseReference  = FirebaseDatabase.instance.ref().child("temperature");
 
 class TemperatureScreen extends StatefulWidget {
   const TemperatureScreen({super.key});
-
 
   @override
   State<TemperatureScreen> createState() => _TemperatureScreenState();
@@ -22,17 +18,21 @@ class _TemperatureScreenState extends State<TemperatureScreen> {
   MQTTClientManager mqttClientManager = MQTTClientManager();
   final String pubTopic = "aquafatec/tanque1/temperatura";
   String temperature = "0.0 ºC";
+  int nextRowNumber = 1;
+  late final DatabaseReference databaseReference;
 
   @override
   void initState() {
+    databaseReference = FirebaseDatabase.instance.ref().child("temperature");
     setupMqttClient();
     setupUpdatesListener();
     super.initState();
   }
-  
+
   @override
   Widget build(BuildContext context) {
-    String formattedDate = DateFormat('dd/MM/yyyy HH:mm:ss').format(DateTime.now());
+    String formattedDate = DateFormat('dd/MM/yyyy HH:mm:ss').format(
+        DateTime.now());
 
     return Scaffold(
       backgroundColor: MyColors.color4,
@@ -52,37 +52,38 @@ class _TemperatureScreenState extends State<TemperatureScreen> {
             Hero(
               tag: 'temperature_image',
               child: Image.asset(
-                'assets/ph_mensal.png', //alterar imagem para algo de temperatura
+                'assets/ph_mensal.png',
+                //alterar imagem para algo de temperatura
                 width: double.infinity,
                 height: 250,
                 alignment: Alignment.center,
               ),
             ),
 
-
             const SizedBox(height: 10),
             _buildInfoBox('Última leitura:', formattedDate, fontSize: 16),
             _buildInfoBox('Valor da leitura:', temperature, fontSize: 16),
             _buildInfoBox('Status do sensor:', 'ATIVO', fontSize: 16),
           ],
+        ),
       ),
-    ),
-    bottomNavigationBar: BottomNavBar(
-      currentIndex: 1,
-      onTap: (menu) {
-        if (menu == 0) {
-          Navigator.pushNamed(context, '/home');
-        } else if (menu == 1) {
-          Navigator.pushNamed(context, '/module');
-        } else if (menu == 2) {
-          Navigator.pushNamed(context, '/notifications');
-        } else if (menu == 3) {
-          Navigator.pushNamed(context, '/profile');
-        }
-      },
-    ),
+      bottomNavigationBar: BottomNavBar(
+        currentIndex: 1,
+        onTap: (menu) {
+          if (menu == 0) {
+            Navigator.pushNamed(context, '/home');
+          } else if (menu == 1) {
+            Navigator.pushNamed(context, '/module');
+          } else if (menu == 2) {
+            Navigator.pushNamed(context, '/notifications');
+          } else if (menu == 3) {
+            Navigator.pushNamed(context, '/profile');
+          }
+        },
+      ),
     );
   }
+
   Widget _buildInfoBox(String label, String value, {double fontSize = 16}) {
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 8.0),
@@ -117,35 +118,39 @@ class _TemperatureScreenState extends State<TemperatureScreen> {
               fontWeight: FontWeight.bold,
               color: Colors.black,
             ),
-          ),   // Cor do texto
+          ), // Cor do texto
         ],
       ),
     );
   }
-  
-  Future<void> setupMqttClient() async{
+
+  Future<void> setupMqttClient() async {
     await mqttClientManager.connect();
     mqttClientManager.subscribe(pubTopic);
   }
-  
-  void setupUpdatesListener(){
+
+  void setupUpdatesListener() {
     mqttClientManager
         .getMessagesStream()!
         .listen((List<MqttReceivedMessage<MqttMessage?>>? c) {
-          final recMess = c![0].payload as MqttPublishMessage;
-          final pt = 
-              MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
-          setState(() {
-            temperature = '$pt ºC';
+      final recMess = c![0].payload as MqttPublishMessage;
+      final pt = MqttPublishPayload.bytesToStringAsString(
+          recMess.payload.message);
 
-            databaseReference.set({"timestamp": DateTime.now(), "value": pt});
+      setState(() {
+        temperature = '$pt ºC';
+        final parsedTimestamp = DateTime.now();
+        final formatter = DateFormat('dd/MM/yyyy HH:mm:ss');
+        final formattedTimestamp = formatter.format(parsedTimestamp);
+        final rowRef = databaseReference.push();
+        rowRef.set({
+          "row_number": nextRowNumber++,
+          "timestamp": formattedTimestamp,
+          "value": pt
+        });
+      });
 
-          });
-          //print('MQTTClient::Message received on topic: <${c[0].topic}> is $pt\n ');
+      //print('MQTTClient::Message received on topic: <${c[0].topic}> is $pt\n ');
     });
   }
-
-  }
-
-
-
+}
